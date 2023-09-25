71aa3570320ea6b457675d6e40b954a36230966e
Damian5314 <113626104+Damian5314@users.noreply.github.com>
2023-09-25 23:48:30 +0200

    Add files via upload

---
+++ rockpaperscissor.py
@@ -0,0 +1,21 @@
+import random
+
+player = input("rock, paper, scissor: ")
+
+opponent =["rock","paper", "scissor"]
+opponent = random.choice(opponent)
+print("opponent: "+ opponent)
+
+if player == "rock" and opponent=="scissor":
+    print("You won")
+elif player == "scissor" and opponent=="paper":
+    print("You won")
+elif player == "paper" and opponent=="rock":
+    print("You won")
+elif player == "rock" and opponent=="paper":
+    print("You lost")
+elif player == "paper" and opponent=="scissor":
+    print("You lost")
+elif splayer == "scissor" and opponent=="rock":
+    print("You lost")
+else: print("Draw")
